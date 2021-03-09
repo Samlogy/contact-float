7c3f14fc185e70fda88caf81d1a43ccf64c4c932
Samlogy <senanisammy@gmail.com>
2021-03-09 21:03:50 +0100

    styled form & toggle button - form - toggle button feature implemented

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
 import { useState } from 'react';
-import { FiShoppingCart } from 'react-icons/fi';
+import { GrFormClose } from 'react-icons/gr';
+import { AiFillMessage } from 'react-icons/ai'
 
 import './style.css';
 
 const App = () => {
-  const [contact, setContact] = useState({ email: '', subject: '', message: '', checked: true });
+  const [contact, setContact] = useState({ email: '', subject: '', message: '' });
+  const [form, setForm] = useState({ show: false });
+
+  const Contact = (e) => {
+    e.preventDefault();
+
+    console.log(contact)
+  }
+
+
 
   return (
     <div className="contact-float-container">
-      <div className='contact-box'>
+      <div className={form.show ? 'contact-box' : 'contact-box-hide'}>
         <div className='contact-box-header'> 
             <h5> Moderator name </h5>
-            <GrFormClose className='icon' />
+            <GrFormClose className='icon' onClick={() => setForm({...form, show: false })} />
         </div>
 
-        <div className='contact-box-body'> 
+        <div className='contact-box-body' onSubmit={Contact}> 
             <form method='POST'>
-                <input type='text' name='email' value={contact.email} placeholder='Email'
+                <input type='email' name='email' value={contact.email} placeholder='Email'
                         onChange={e => setContact({...contact, email: e.target.value})} />
 
-                <input type='text' name='email' value={contact.email} placeholder='Email'
-                        onChange={e => setContact({...contact, email: e.target.value})} />
+                <input type='text' name='subject' value={contact.subject} placeholder='Subject'
+                        onChange={e => setContact({...contact, subject: e.target.value})} />
 
-                <input type='text' name='email' value={contact.email} placeholder='Email'
-                        onChange={e => setContact({...contact, email: e.target.value})} />
-
-                <textarea name='email' value={contact.email} placeholder='Email'
-                        onChange={e => setContact({...contact, email: e.target.value})}></textarea>
+                <textarea name='message' value={contact.message} placeholder='Message'
+                        onChange={e => setContact({...contact, message: e.target.value})}></textarea>
             </form>
         </div>
 
         <div className='contact-box-footer'> 
-            <button className='btn'> Send </button>
+            <button className='btn btn-accent' onClick={(e) => Contact(e)}> Send </button>
         </div>
       </div>
+
+      <button className='btn btn-icon' onClick={() => setForm({...form, show: !form.show })}>
+        { form.show ? <GrFormClose /> : <AiFillMessage /> }
+      </button>
     </div>
   );
 }
