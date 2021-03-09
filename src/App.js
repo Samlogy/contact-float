4a0c35df4ebf41803195fa6b01add8de81569e15
Samlogy <senanisammy@gmail.com>
2021-03-09 20:14:16 +0100

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
 
-function App() {
+import './style.css';
+
+const App = () => {
+  const [contact, setContact] = useState({ email: '', subject: '', message: '', checked: true });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contact-float-container">
+      <div className='contact-box'>
+        <div className='contact-box-header'> 
+            <h5> Moderator name </h5>
+            <GrFormClose className='icon' />
+        </div>
+
+        <div className='contact-box-body'> 
+            <form method='POST'>
+                <input type='text' name='email' value={contact.email} placeholder='Email'
+                        onChange={e => setContact({...contact, email: e.target.value})} />
+
+                <input type='text' name='email' value={contact.email} placeholder='Email'
+                        onChange={e => setContact({...contact, email: e.target.value})} />
+
+                <input type='text' name='email' value={contact.email} placeholder='Email'
+                        onChange={e => setContact({...contact, email: e.target.value})} />
+
+                <textarea name='email' value={contact.email} placeholder='Email'
+                        onChange={e => setContact({...contact, email: e.target.value})}></textarea>
+            </form>
+        </div>
+
+        <div className='contact-box-footer'> 
+            <button className='btn'> Send </button>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
